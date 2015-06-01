38f606e14109c185e62e099283cc78de900d106e
silverk <silverk@silverk.local>
2015-06-01 20:06:34 +0800

    测试github所用

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by silverk on 15/6/1.
+//  Copyright (c) 2015年 silverk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by silverk on 15/6/1.
+//  Copyright (c) 2015年 silverk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
